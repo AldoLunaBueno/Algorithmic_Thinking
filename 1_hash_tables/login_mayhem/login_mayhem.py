472d280dc0f04d3af8e4ad6c6a88d66bcc673411
AldoLunaBueno <alunab@uni.pe>
2024-09-02 09:22:41 -0500

    Solved by brute force Time complexity: O(n**2)

--- 1_hash_tables/login_mayhem/login_mayhem.py
+++ 1_hash_tables/login_mayhem/login_mayhem.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Tuple
 
 def main():
     q, queries = get_data()
-    solve_by_naive()
+    solve_by_naive(q, queries)
 
-def get_data():
+def get_data() -> Tuple[int, List[Tuple[int, str]]]:
     q = int(input())
     queries: List[str] = [None] * q
     for i in range(q):
@@ -13,8 +13,22 @@ def get_data():
     queries = [(int(query[0]), query[1]) for query in queries]
     return q, queries
 
-def solve_by_naive():
-    pass
+# TIME: O(**2)
+def solve_by_brute_force(q: int, queries: List[Tuple[int, str]]):
+    pass_db = [] # database of passwords
+    for query in queries: # time: O(n**2)
+        pass_user = query[1]
+
+        if query[0] == 1:
+            pass_db.append(pass_user)
+            continue
+
+        # query[0] == 2
+        violation_count = 0
+        for p in pass_db: # time: O(n)
+            if pass_user in p:
+                violation_count += 1
+        print(f"{violation_count}")
 
 if __name__ == "__main__":
     main()
