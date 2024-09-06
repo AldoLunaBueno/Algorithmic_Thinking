db81682e277c305f8133d59bf727d95d1bba1f24
AldoLunaBueno <alunab@uni.pe>
2024-09-06 17:08:19 -0500

    solved by brute force (but failed) "time limit exceeded" according to the SPOJ judge

--- 8_heaps_and_segment_trees/promotion/promotion.py
+++ 8_heaps_and_segment_trees/promotion/promotion.py
@@ -1,5 +1,35 @@
+# Problem: Promotion
+# Judge: SPOJ
+# ID: PRO
+
+from typing import List
+
 def main():
-    pass
+    receipts = get_data()
+    solve_by_brute_force(receipts)
+
+def get_data() -> List[List[int]]:
+    n = int(input())
+    receipts = []
+    for _ in range(n):
+        line = input()
+        k, *day_receipts = line.split(" ")
+        day_receipts = [int(r) for r in day_receipts]
+        receipts.append(day_receipts)
+    return receipts
+
+def solve_by_brute_force(receipts: List[List[int]]):
+    total_prize = 0
+    acc_receipts = []
+    for day_receipts in receipts:
+        acc_receipts.extend(day_receipts)
+        minimum = min(acc_receipts)
+        maximum = max(acc_receipts)
+        acc_receipts.remove(minimum)
+        acc_receipts.remove(maximum)
+        prize = maximum - minimum
+        total_prize += prize
+    print(total_prize)
 
 if __name__ == "__main__":
     main()
--- 8_heaps_and_segment_trees/promotion/test_promotion.py
+++ 8_heaps_and_segment_trees/promotion/test_promotion.py
@@ -8,7 +8,7 @@ parametrize = pytest.mark.parametrize
 
 
 def _test(capsys: pytest.CaptureFixture[str], input, expected):
-    input = "\n".join(input)
+    input = "\n".join(input) + "\n"
     sys.stdin = io.StringIO(input)
     promotion.main()
     out, err = capsys.readouterr()
@@ -16,7 +16,8 @@ def _test(capsys: pytest.CaptureFixture[str], input, expected):
 
 @parametrize("input, expected",
              [
-                 (("2", "16 6 63 16 82 25 2 43 5 17 10 56 85 38 15 32 91", "1 57"), "169")
+                 (("2", "16 6 63 16 82 25 2 43 5 17 10 56 85 38 15 32 91", "1 57"), "169\n"), 
+                 (("5", "3 1 2 3", "2 1 1", "4 10 5 5 1", "0", "1 2"), "19\n")
                  
              ])
 def test_complex(capsys: pytest.CaptureFixture[str], input, expected):
