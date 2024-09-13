52bc35ac3a40ddc0e4d8abd13668c1c04c29b8ff
AldoLunaBueno <alunab@uni.pe>
2024-09-13 15:25:22 -0500

    get and structure data

--- 7_binary_search/feeding_ants/feeding_ants.py
+++ 7_binary_search/feeding_ants/feeding_ants.py
@@ -1,12 +1,31 @@
-# Problem: Feeding ants
-# Judge: DMOJ
-# ID: coci14c4p4
+# Feeding ants
+# DMOJ coci14c4p4 problem
+
+from typing import List, Dict, Tuple
+
+EdgeDict = Dict[int, List[int]]
 
 def main():
-    print(10.001)
+    E, V = get_data()
+    solve(E, V)
 
-def get_data():
+def get_data() -> Tuple[EdgeDict, List[int]]:
+    """
+    Get data and return it like an adjacency list of edges 
+    plus a list of node values.
+    """
+    n = int(input())
+    edges: EdgeDict = {}
+    edges = { i:[] for i in range(1, n+1) }
+    for _ in range(n-1):
+        edge = input().split(" ")
+        father, son, percent, is_super = [int(x) for x in edge]
+        edges[father].append((son, percent, is_super))
+    node_values = [None] + [int(x) for x in input().split(" ")]
+    return edges, node_values
+
+def solve(edges: EdgeDict, node_values: List[int]):
     pass
 
-def solve():
-    pass
+if __name__ == "__main__":
+    main()
