3a234c096f5168f2be63edc934b62f854410dbe4
AldoLunaBueno <alunab@uni.pe>
2024-09-15 22:38:01 -0500

    Greedy alone fails, and it'll always does

--- 7_binary_search/river_hopscotch/river_hopscotch.py
+++ 7_binary_search/river_hopscotch/river_hopscotch.py
@@ -1,5 +1,57 @@
 # River Hopscotch
 # POJ problem 3258
 
+from typing import Tuple, List
+
+MAX_RIVER_WIDTH = 1_000_000_000
+MAX_ROCKS = 50_000
+ROCKS_TO_REMOVE = MAX_ROCKS
+
 def main():
-    print(0)
+    m, rocks = get_data()
+    solve_by_greedy(m, rocks)
+
+def get_data() -> Tuple[List[int], int]:
+    L, n, m = [int(x) for x in input().split(" ")]
+    rocks = [0]
+    for _ in range(n):
+        rocks.append(int(input()))
+    rocks.append(L)
+    return m, rocks
+
+def solve_by_greedy(m: int, rocks: List[int]):
+    rocks = sorted(rocks)
+    min_jump = float("Inf")
+    
+    for _ in range(m):
+        min_jump, min_index = get_min_jump(rocks)
+        if min_index == 0:
+            rocks.pop(min_index+1)
+            continue
+        elif min_index == len(rocks)-2:
+            rocks.pop(min_index)
+            continue
+        
+        neighbor_1 = rocks[min_index] - rocks[min_index-1]
+        neighbor_2 = rocks[min_index+2] - rocks[min_index+1]
+        if neighbor_1 < neighbor_2:
+            rocks.pop(min_index)
+        else:
+            rocks.pop(min_index+1)
+
+    min_jump, _ = get_min_jump(rocks)
+    print(min_jump)
+
+def get_min_jump(rocks):
+    n = len(rocks)
+    min_jump = float("Inf")
+    min_index = -1        
+    for i in range(n-1):
+        jump = rocks[i+1] - rocks[i]
+        if jump < min_jump:
+            min_jump = jump
+            min_index = i
+    return min_jump, min_index
+
+if __name__ == "__main__":
+    main()
--- 7_binary_search/river_hopscotch/test_river_hopscotch.py
+++ 7_binary_search/river_hopscotch/test_river_hopscotch.py
@@ -13,7 +13,10 @@ def _test(capsys: pytest.CaptureFixture[str], input, expected):
     assert out == expected + "\n"
 
 @parametrize("input, expected", [
-    (("25 5 2", "14", "11", "21", "17"), "4")
+    (("12 2 1", "5", "8"), "5"),
+    (("12 4 2", "1", "3", "8", "9"), "3"),
+    (("25 5 2", "2", "14", "11", "21", "17"), "4"),
+    (("12 4 2", "2", "4", "5", "8"), "4")
 ])
 def test_complex(capsys: pytest.CaptureFixture[str], input, expected):
     _test(capsys, input, expected)
