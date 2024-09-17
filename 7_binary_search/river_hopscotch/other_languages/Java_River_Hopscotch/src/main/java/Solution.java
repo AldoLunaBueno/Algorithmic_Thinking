e3ec1861abda84f0ce14cd805c6c96d07cf04c64
AldoLunaBueno <alunab@uni.pe>
2024-09-16 20:15:00 -0500

    java test in red

---
+++ 7_binary_search/river_hopscotch/other_languages/Java_River_Hopscotch/src/main/java/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int L = Integer.parseInt(scanner.next());
+        int n = Integer.parseInt(scanner.next());
+        int m = Integer.parseInt(scanner.next());
+        System.out.println();
+        scanner.close();
+
+    }
+}
---
+++ 7_binary_search/river_hopscotch/other_languages/Java_River_Hopscotch/src/test/java/SolutionTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+class SolutionTest {
+    @ParameterizedTest
+    @MethodSource("testMain")
+    void testMain(String input, String expected) throws IOException {
+        provideInput(input);
+        OutputStream out = receiveOutput();
+        Solution.main(null);
+        String actual = out.toString();
+        assertEquals(expected, actual);
+    }
+    static Stream<Arguments> testMain() {
+        String nl = System.lineSeparator();
+        return Stream.of(
+                Arguments.of("12 2 1\n5\n8\n", "5" + nl),
+                Arguments.of("12 4 2\n1\n3\n8\n9\n", "3" + nl),
+                Arguments.of("25 5 2\n2\n14\n11\n21\n17\n", "4" + nl),
+                Arguments.of("12 4 2\n2\n4\n5\n8\n", "4" + nl)
+        );
+    }
+    private void provideInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+    private OutputStream receiveOutput() {
+        OutputStream stdout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(stdout));
+        return stdout;
+    }
+}
