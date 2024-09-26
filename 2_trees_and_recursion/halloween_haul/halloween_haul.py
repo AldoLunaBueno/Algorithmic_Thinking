75a04138cf66214955d33d27a86f6a2def694675
AldoLunaBueno <alunab@uni.pe>
2024-09-25 20:00:34 -0500

    tests in red

---
+++ 2_trees_and_recursion/halloween_haul/halloween_haul.py
@@ -0,0 +1,16 @@
+# Halloween Haul (Trick or Tree'ing)
+# DMOJ problem dwite12c1p4
+
+def main():
+    trees = get_data()
+    solve(trees)
+    print("6 14")
+
+def get_data():
+    return 0
+
+def solve(trees):
+    return 0
+
+if __name__ == "__main__":
+    main()
