f72cecff4084fdb01f127096ed218dda2e8f33d6
AldoLunaBueno <alunab@uni.pe>
2024-09-25 23:57:29 -0500

    tree sum by recursion and by iteration

---
+++ 2_trees_and_recursion/halloween_haul/trees.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Node:
+    def __init__(self, value: int = 0, left: "Node" = None, right: "Node" = None) -> None:
+        self.value = value
+        self.right = right
+        self.left =  left
+    def tree_sum_by_recursion(self: "Node"):
+        if self.left == self.right == None:
+            return self.value
+        value_l = self.left.tree_sum_by_recursion()
+        value_r = self.right.tree_sum_by_recursion()
+        return value_l + value_r
+
+def tree_sum_by_iteration(root: Node):
+    result = 0
+    stack: List[Node] = [None]
+    curr_node: Node = root
+    while True:
+        if curr_node.left == curr_node.right == None: # leaf
+            result += curr_node.value
+            curr_node = stack.pop()
+            if curr_node == None:
+                break
+            continue
+        stack.append(curr_node.left)
+        curr_node = curr_node.right
+        
+    return result
+
+four = Node(4)
+nine = Node(9)
+b = Node(left=four, right=nine)
+fifteen = Node(15)
+c = Node(left=b, right=fifteen)
+
+print(c.tree_sum_by_recursion())
+print(tree_sum_by_iteration(c))
