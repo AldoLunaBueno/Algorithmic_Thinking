8944e8789a734865cf5e0a05d25564b5f437104b
AldoLunaBueno <alunab@uni.pe>
2024-09-30 15:32:17 -0500

    SOLVED!!! The integer sample generator was consuming a lot of time. We replace it by a index generator with a fixed step.

--- 10_randomization/yokan/yokan.py
+++ 10_randomization/yokan/yokan.py
@@ -1,15 +1,17 @@
 # Yokan
 # DMOJ problem dmpg15g6
 
-# Resources: ---, 117.94 MB
-# Final score: 60/100 (10.2/17 points)
+# Resources: 11.375s, 120.75 MB
+# Maximum single-case runtime: 1.202s
+# Final score: 100/100 (17.0/17 points)
 
 from typing import List
-from random import sample
-from collections import defaultdict
+from collections import defaultdict, Counter
 from sys import stdin
+from bisect import bisect_left, bisect_right
 
-MAX_ATTEMPTS = 7
+SAMPLE_SIZE = 30
+MAX_ATTEMPTS = 3
 
 def main():
     n, m, yokan, queries = get_data()
@@ -27,15 +29,16 @@ def solve(n, m, yokan, queries):
     pieces_by_flavor = defaultdict(list)
     for i, flavor in enumerate(yokan):
         pieces_by_flavor[flavor].append(i)
-      
+    
     for l, r in queries:
         width = r-l+1
         one_third = width/3
         happy_count = 0
-        pieces_sample = sample(range(l, r+1), min(width, MAX_ATTEMPTS))
-        rand_flavors = set([yokan[i] for i in pieces_sample])
+        step = max((r-l+1) // min(width, SAMPLE_SIZE), 1)
+        pieces_sample = range(l, r+1, step)
+        counter = Counter([yokan[i] for i in pieces_sample])
 
-        for flavor in rand_flavors:
+        for flavor, _ in counter.most_common(MAX_ATTEMPTS):
             slab_by_flavor = pieces_by_flavor[flavor]
             one_flavor_width = flavor_range(l, r, slab_by_flavor)
             if one_flavor_width >= 2 * one_third:
@@ -49,11 +52,9 @@ def solve(n, m, yokan, queries):
         print("YES" if happy_count == 2 else "NO")
 
 def flavor_range(l, r, arr: List):
-    li = binary_search(l, arr)
-    if li == -1 or arr[li] < l:
-        li += 1
-    ri = binary_search(r, arr)
-    return ri - li + 1
+    li = bisect_left(arr, l)
+    ri = bisect_right(arr, r)
+    return ri - li
 
 def binary_search(element, arr: List):
     low, high = -1, len(arr)
