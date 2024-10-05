2b9f26816c9a2d46aec85ef61f93d20dd0273b79
AldoLunaBueno <alunab@uni.pe>
2024-10-04 19:54:48 -0500

    test in red

---
+++ 5_graphs_and_bfs/knight_chase/algorithms/own_idea.py
@@ -0,0 +1,4 @@
+from typing import List
+
+def solve(cases: List):
+    print("Win in 3 knight move(s).")
---
+++ 5_graphs_and_bfs/knight_chase/knight_chase.py
@@ -0,0 +1,25 @@
+# Knight Chase (A Knightly Pursuit)
+# DMOJ problem ccc99s4
+
+from algorithms.own_idea import solve
+
+def main():
+    cases = get_data()
+    solve(cases)
+
+def get_data():
+    n = int(input())
+    cases = []
+    for _ in range(n):
+        r =  int(input())
+        c =  int(input())
+        pr = int(input())
+        pc = int(input())
+        kr = int(input())
+        kc = int(input())
+        cases.append((r, c, pr, pc, kr, kc))
+    return cases
+        
+
+if __name__ == "__main__":
+    main()
