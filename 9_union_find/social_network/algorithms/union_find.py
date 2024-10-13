ea2917af024d4caa8215e71b3b8a1eba61b9ace3
AldoLunaBueno <alunab@uni.pe>
2024-10-13 14:03:48 -0500

    design of solution

---
+++ 9_union_find/social_network/algorithms/union_find.py
@@ -0,0 +1,28 @@
+from typing import List, Tuple
+
+class UnionFind:
+    def __init__(self, num_nodes):
+        _nodes = [-1] * num_nodes
+        _sizes = [1] * num_nodes
+
+    def union(self, node_1, node_2):
+        pass
+
+    def find(self, node):
+        pass
+
+    def size(self, node):
+        pass
+
+def solve(n: int, m: int, q: List[Tuple]):
+
+    network = UnionFind(n+1)
+
+def add(network: UnionFind, person_1: int, person_2: int):
+    pass
+
+def examine(network: UnionFind, person_1: int, person_2: int) -> bool:
+    pass
+
+def size(network: UnionFind, person: int) -> int:
+    pass
